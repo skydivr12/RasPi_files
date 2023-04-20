66b1508258736593dbb7e603fe81bc5a1666aa91
skydivr12 <79960801+skydivr12@users.noreply.github.com>
2023-04-19 19:07:56 -0500

    Create light_sense.py
    Python file to automatically adjust backlight brightness of the pi display. 
    Hardware required:
    MCP3008 ADC
    10k resistor
    light sensor
    5 jumper wires
    
    Based on this tutorial:
    https://tutorials-raspberrypi.com/photoresistor-brightness-light-sensor-with-raspberry-pi/

---
+++ light_sense.py
@@ -0,0 +1,48 @@
+import os
+import time
+from spidev import SpiDev
+ 
+class MCP3008:
+    def __init__(self, bus = 0, device = 0):
+        self.bus, self.device = bus, device
+        self.spi = SpiDev()
+        self.open()
+        self.spi.max_speed_hz = 1000000 # 1MHz
+ 
+    def open(self):
+        self.spi.open(self.bus, self.device)
+        self.spi.max_speed_hz = 1000000 # 1MHz
+    
+    def read(self, channel = 0):
+        adc = self.spi.xfer2([1, (8 + channel) << 4, 0])
+        data = ((adc[1] & 3) << 8) + adc[2]
+        return data
+
+    def close(self):
+        self.spi.close()
+ 
+# Define variables
+adc = MCP3008()
+min_light_value = 100
+max_light_value = 900
+min_backlight_value = 15
+max_backlight_value = 100
+delay_time = 3
+
+while True:
+    # Read light sensor value
+    light_value = adc.read(channel=0)
+
+    # Map light sensor value to backlight value
+    if light_value < min_light_value:
+        backlight_value = min_backlight_value
+    elif light_value > max_light_value:
+        backlight_value = max_backlight_value
+    else:
+        backlight_value = (light_value - min_light_value) * (max_backlight_value - min_backlight_value) / (max_light_value - min_light_value) + min_backlight_value
+
+    # Set backlight value
+    os.system("vcgencmd set_backlight {}".format(int(backlight_value)))
+
+    # Delay
+    time.sleep(delay_time)
